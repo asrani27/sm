2ab76619059e7351414f564ded1e17768f627bc7
asrani <asrani.27@gmail.com>
2023-12-28 17:32:09 +0800

    f

--- app/Http/Controllers/WAController.php
+++ app/Http/Controllers/WAController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Nomor;
+use GuzzleHttp\Client;
 use App\Models\Whatsapp;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
@@ -18,15 +19,56 @@ class WAController extends Controller
     public function sendMessage(Request $req)
     {
         $nomor = Nomor::get();
+        $file      = $req->file('file');
+        $api_url   = 'http://103.178.83.200:8000/send-message';
+        if ($file == null) {
+            foreach ($nomor as $n) {
+                $client = new Client();
+                $response = $client->request("POST", $api_url, [
+                    'multipart' => [
+                        [
+                            'name' => 'message',
+                            'contents' => $req->message
+                        ],
+                        [
+                            'name' => 'number',
+                            'contents' => $n->nomor,
+                        ],
+                    ]
+                ]);
+                $code = $response->getStatusCode();
+                sleep(5);
+            }
+            return back();
+        } else {
+            $file_path = $file->getPathname();
+            $file_mime = $file->getMimeType('image');
+            $file_name = $file->getClientOriginalName();
 
-        foreach ($nomor as $n) {
-            $response = Http::post('http://103.178.83.200:8000/send-message', [
-                'message' => $req->message,
-                'number' => $n->nomor,
-                'file_dikirim' => $req->file
-            ]);
-            sleep(5);
+            foreach ($nomor as $n) {
+                $client = new Client();
+                $response = $client->request("POST", $api_url, [
+                    'multipart' => [
+                        [
+                            'name' => 'message',
+                            'contents' => $req->message
+                        ],
+                        [
+                            'name' => 'number',
+                            'contents' => $n->nomor,
+                        ],
+                        [
+                            'name' => 'file_dikirim',
+                            'filename' => $file_name,
+                            'Mime-Type' => $file_mime,
+                            'contents' => fopen($file_path, 'r')
+                        ],
+                    ]
+                ]);
+                $code = $response->getStatusCode();
+                sleep(5);
+            }
+            return back();
         }
-        return back();
     }
 }
