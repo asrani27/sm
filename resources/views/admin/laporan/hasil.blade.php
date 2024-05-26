c06bca3e446ac3a078acac57801764314bb30de6
asrani <asrani.27@gmail.com>
2024-05-26 11:36:37 +0800

    d

--- resources/views/admin/laporan/hasil.blade.php
+++ resources/views/admin/laporan/hasil.blade.php
@@ -35,5 +35,12 @@
 </table>
 
 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+<script>
+
+$(document).ready(function(){
+    window.print();
+});
+</script>
 </body>
 </html>
