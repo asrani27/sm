717180cc365bf9c563e8f9a4808b5b541d9a4767
asrani <asrani.27@gmail.com>
2024-05-26 10:42:56 +0800

    f

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -14,14 +14,14 @@
     <li class="{{ (request()->is('superadmin/user*')) ? 'active' : '' }}"><a href="/superadmin/user"><i class="fa fa-list"></i> <span>Data User</span></a></li>
     <li class="{{ (request()->is('superadmin/kecamatan*')) ? 'active' : '' }}"><a href="/superadmin/kecamatan"><i class="fa fa-users"></i> <span>Koordinator Kecamatan</span></a></li>
     <li class="{{ (request()->is('superadmin/kelurahan*')) ? 'active' : '' }}"><a href="/superadmin/kelurahan"><i class="fa fa-users"></i> <span>Koordinator Kelurahan</span></a></li>
-    <li class="{{ (request()->is('superadmin/koordinatortps*')) ? 'active' : '' }}"><a href="/superadmin/koordinatortps"><i class="fa fa-users"></i> <span>Koordinator TPS</span></a></li>
+    {{-- <li class="{{ (request()->is('superadmin/koordinatortps*')) ? 'active' : '' }}"><a href="/superadmin/koordinatortps"><i class="fa fa-users"></i> <span>Koordinator TPS</span></a></li> --}}
     <li class="{{ (request()->is('superadmin/ketuart*')) ? 'active' : '' }}"><a href="/superadmin/ketuart"><i class="fa fa-users"></i> <span>KETUA RT</span></a></li>
     {{-- <li class="{{ (request()->is('superadmin/kecamatan*')) ? 'active' : '' }}"><a href="/superadmin/kecamatan"><i class="fa fa-list"></i> <span>Data Kecamatan</span></a></li>
     <li class="{{ (request()->is('superadmin/kelurahan*')) ? 'active' : '' }}"><a href="/superadmin/kelurahan"><i class="fa fa-list"></i> <span>Data Kelurahan</span></a></li>
     <li class="{{ (request()->is('superadmin/rt*')) ? 'active' : '' }}"><a href="/superadmin/rt"><i class="fa fa-list"></i> <span>Data RT</span></a></li> --}}
     <li class="{{ (request()->is('superadmin/sm*')) ? 'active' : '' }}"><a href="/superadmin/sm"><i class="fa fa-users"></i> <span>Data SM</span></a></li>
-    <li class="{{ (request()->is('superadmin/saksi*')) ? 'active' : '' }}"><a href="/superadmin/saksi"><i class="fa fa-users"></i> <span>Data Saksi</span></a></li>
-    <li class="{{ (request()->is('superadmin/timses*')) ? 'active' : '' }}"><a href="/superadmin/timses"><i class="fa fa-users"></i> <span>Data Timses Non Koordinator</span></a></li>
+    {{-- <li class="{{ (request()->is('superadmin/saksi*')) ? 'active' : '' }}"><a href="/superadmin/saksi"><i class="fa fa-users"></i> <span>Data Saksi</span></a></li>
+    <li class="{{ (request()->is('superadmin/timses*')) ? 'active' : '' }}"><a href="/superadmin/timses"><i class="fa fa-users"></i> <span>Data Timses Non Koordinator</span></a></li> --}}
     <li class="{{ (request()->is('superadmin/survey*')) ? 'active' : '' }}"><a href="/superadmin/survey"><i class="fa fa-comments"></i> <span>Hasil Survey</span></a></li>
     <li class="{{ (request()->is('superadmin/pileg*')) ? 'active' : '' }}"><a href="/superadmin/pileg"><i class="fa fa-th"></i> <span>Hasil Pileg</span></a></li>
     <li class="{{ (request()->is('superadmin/struktur*')) ? 'active' : '' }}"><a href="/superadmin/struktur"><i class="fa fa-th"></i> <span>Struktur Organisasi</span></a></li>
