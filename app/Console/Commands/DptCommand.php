6d9c43fd0cc025b5a7f27afa96ef9f0bf8593d9a
asrani <asrani.27@gmail.com>
2024-07-06 02:12:54 +0800

    f

--- app/Console/Commands/DptCommand.php
+++ app/Console/Commands/DptCommand.php
@@ -41,7 +41,7 @@ class DptCommand extends Command
     public function handle()
     {
 
-        $path = base_path('storage/app/public/dpt/barat.xlsx');
+        $path = base_path('public/assets/barat.xlsx');
         $spreadsheet = IOFactory::load($path);
         $worksheet = $spreadsheet->getActiveSheet();
         $data = $worksheet->toArray();
