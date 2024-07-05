b1d13ea72f1706b76cf56e3e714551cbc116d0e2
asrani <asrani.27@gmail.com>
2024-07-06 03:41:41 +0800

    f

--- resources/views/admin/dpt/index.blade.php
+++ resources/views/admin/dpt/index.blade.php
@@ -11,8 +11,8 @@
             <i class="ion ion-clipboard"></i><h3 class="box-title">Data DPT </h3>
 
             <div class="box-tools">
-              <a href="/superadmin/dpt/delete" class="btn btn-flat btn-sm btn-primary"><i class="fa fa-trash"></i> Delete All</a>
-              <a href="/superadmin/dpt/tarikdpt" class="btn btn-flat btn-sm btn-primary" ><i class="fa fa-refresh"></i> Sync DPT</a>
+              {{-- <a href="/superadmin/dpt/delete" class="btn btn-flat btn-sm btn-primary"><i class="fa fa-trash"></i> Delete All</a>
+              <a href="/superadmin/dpt/tarikdpt" class="btn btn-flat btn-sm btn-primary" ><i class="fa fa-refresh"></i> Sync DPT</a> --}}
             </div>
           </div>
           <!-- /.box-header -->
@@ -21,31 +21,31 @@
               <tbody><tr>
                 <th>No</th>
                 <th>Kecamatan</th>
-                <th>Desa/Kelurahan</th>
+                <th>Kelurahan</th>
+                <th>NIK</th>
                 <th>Nama</th>
-                <th>Usia</th>
-                <th>Jkel</th>
+                <th>Tempat Lahir</th>
                 <th>RT</th>
                 <th>RW</th>
                 <th>TPS</th>
                 
-                <th>Aksi</th>
+                {{-- <th>Aksi</th> --}}
               </tr>
               @foreach ($data as $key => $item)
               <tr>
                 <td>{{$data->firstItem() + $key}}</td>
                 <td>{{$item->kecamatan}}</td>
                 <td>{{$item->kelurahan}}</td>
+                <td>{{$item->nik}}</td>
                 <td>{{$item->nama}}</td>
-                <td>{{$item->usia}}</td>
-                <td>{{$item->jkel}}</td>
+                <td>{{$item->tempat_lahir}}</td>
                 <td>{{$item->rt}}</td>
                 <td>{{$item->rw}}</td>
                 <td>{{$item->tps}}</td>
-                <td>
+                {{-- <td>
                   <a href="/superadmin/dpt/edit/{{$item->id}}" class="btn btn-flat btn-xs btn-primary"><i class="fa fa-edit"></i> Edit</a>
                   <a href="/superadmin/dpt/delete/{{$item->id}}" class="btn btn-flat btn-xs btn-primary" onclick="return confirm('Yakin ingin dihapus?');"><i class="fa fa-trash"></i> Delete</a>
-                </td>
+                </td> --}}
               </tr>
               @endforeach
             </tbody></table>
