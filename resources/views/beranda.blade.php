22f9e9eaadc795b730edff630d21bc1a31028d7a
asrani <asrani.27@gmail.com>
2024-07-27 11:05:01 +0800

    f

--- resources/views/beranda.blade.php
+++ resources/views/beranda.blade.php
@@ -90,7 +90,7 @@
             <div class="row">
               <div class="col-lg-6">
                 <h1 class="text-black">H. Mukhyar</h1>
-                <p class="lead">Calon Walikota Banjarmasin 2024 <br> Bersama Kita Membangun Banua</p>
+                <p class="lead">Calon Walikota Banjarmasin 2024 <br> Banjarmasin Hebat</p>
                 
                 <p><a href="/gabung" class="btn smoothscroll btn-primary">Gabung </a> &nbsp;&nbsp;<a href="https://www.youtube.com/watch?v=IpoiaMwnwKM" class="btn smoothscroll btn-primary" target="_blank">Profil</a> </p>
                 
